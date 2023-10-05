7cf3b712bce390c910d7410377d611b7fac21607
cod3kid <djsufail@gmail.com>
2023-10-05 11:57:06 +0530

    Webpack Plugins: MiniCss Plugin Part 2

--- webpack-plugin/src/index.js
+++ webpack-plugin/src/index.js
@@ -1,4 +1,8 @@
 import Button from "./components/button/button";
+import Paragraph from "./components/paragraph/paragraph";
 
 const button = new Button();
+const paragraph = new Paragraph();
+
 button.render();
+paragraph.render();
